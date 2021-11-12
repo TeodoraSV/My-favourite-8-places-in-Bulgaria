2188d7dce6b3c897ebb64176194d8a8912798367
Teodora <61601741+TeodoraSV@users.noreply.github.com>
2021-11-12 15:07:45 +0200

    Zooming again

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() { 
   noCanvas();
-  var map = L.map('mapid').setView([42.7339, 25.4858],8);
+  var map = L.map('mapid').setView([42.7339, 25.4858],9);
 
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
